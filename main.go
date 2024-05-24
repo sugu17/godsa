cf076d7339eedab3110ec78848f36c0632a749de
Sugumar <sugumar@kttelematic.com>
2024-05-24 08:18:07 +0530

    Batman

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"godsa/list"
+	"godsa/divideandconquer"
+	"log"
 )
 
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
 func main() {
-	list := list.NewSinglyLinkedList[int]()
-	list.PushBack(3)
-	list.PushBack(6)
-	list.PushBack(10)
-	list.PushFront(19)
-	list.Remove(3)
 }
