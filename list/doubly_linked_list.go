6636d4a457e50e91e3e38a8583f59284032b4c96
Wade Wilson <sugumarsg17@gmail.com>
2024-05-25 07:39:07 +0530

    Added PushBack, IndexOf, Remove methods to the doublylinked list API

--- list/doubly_linked_list.go
+++ list/doubly_linked_list.go
@@ -24,8 +24,9 @@ func (dln *doublyLinkedNode[T]) Prev() *doublyLinkedNode[T] {
 }
 
 type DoublyLinkedList[T any] struct {
-	head *doublyLinkedNode[T]
-	tail *doublyLinkedNode[T]
+	head   *doublyLinkedNode[T]
+	tail   *doublyLinkedNode[T]
+	length int
 }
 
 func (dll *DoublyLinkedList[T]) Front() *doublyLinkedNode[T] {
@@ -52,12 +53,83 @@ func (dll *DoublyLinkedList[T]) PushFront(value T) *doublyLinkedNode[T] {
 	if head == nil {
 		dll.head = newNode
 		dll.tail = newNode
+		dll.length += 1
 		return newNode
 	}
 	newNode.next = head
 	head.prev = newNode
 	dll.head = newNode
+	dll.length += 1
 	return newNode
+}
+
+func (dll *DoublyLinkedList[T]) PushBack(value T) *doublyLinkedNode[T] {
+	tail := dll.Back()
+	newNode := &doublyLinkedNode[T]{
+		next:  nil,
+		prev:  nil,
+		Value: value,
+	}
+	if tail == nil {
+		dll.head = newNode
+		dll.tail = newNode
+		dll.length += 1
+		return newNode
+	}
+	tail.next = newNode
+	newNode.prev = tail
+	dll.tail = newNode
+	dll.length += 1
+	return newNode
+}
+
+func (dll *DoublyLinkedList[T]) IndexOf(test func(val T) bool) (int, bool) {
+	index := 0
+	for node := dll.Front(); node != nil; node = node.Next() {
+		if test(node.Value) == true {
+			return index, true
+		}
+		index++
+	}
+	return -1, false
+}
+
+func (dll *DoublyLinkedList[T]) Remove(index int) (T, bool) {
+	var defaultValue T
+	if !(index >= 0 && index < dll.length) {
+		return defaultValue, false
+	}
+	if index == 0 {
+		nodeNextToHead := dll.head.next
+		nodeNextToHead.prev = nil
+		nodeVal := dll.head.Value
+		dll.head = nodeNextToHead
+		dll.length -= 1
+		return nodeVal, true
+	}
+	if index == dll.length-1 {
+		nodeBeforeTail := dll.tail.prev
+		nodeBeforeTail.next = nil
+		nodeVal := dll.tail.Value
+		dll.tail = nodeBeforeTail
+		dll.length -= 1
+		return nodeVal, true
+	}
+	cur := 1
+	for node := dll.Front().Next(); node.Next() != nil; {
+		if index == cur {
+			nodeBeforeTarget := node.Prev()
+			nodeAfterTarget := node.Next()
+			nodeVal := node.Value
+			nodeBeforeTarget.next = nodeAfterTarget
+			nodeAfterTarget.prev = nodeBeforeTarget
+			node = nil
+			dll.length -= 1
+			return nodeVal, true
+		}
+		cur++
+	}
+	return defaultValue, false
 }
 
 func (dll DoublyLinkedList[T]) String() string {
@@ -75,7 +147,8 @@ func (dll DoublyLinkedList[T]) String() string {
 
 func NewDoublyLinkedList[T any]() DoublyLinkedList[T] {
 	return DoublyLinkedList[T]{
-		head: nil,
-		tail: nil,
+		head:   nil,
+		tail:   nil,
+		length: 0,
 	}
 }
